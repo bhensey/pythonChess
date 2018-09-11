dc85b91fb7ee7f8b3822c9d14a25e801e8e71163
Bennett Hensey <bennett.hensey@gmail.com>
2018-09-11 17:39:47 -0500

    Added check features to move validation

--- GUI.py
+++ GUI.py
@@ -2,62 +2,57 @@ import tkinter as tk
 from rules import *
 
 #Global Variables
-tiles = ["floral white","peachpuff3"]
-tiles = (tiles*4+tiles[::-1]*4)*4
-squareList=[]
+class GUI:
+	tiles = ["peachpuff3","floral white"]
+	tiles = (tiles*4+tiles[::-1]*4)*4
+
+	def __init__(self, board):
+		self.window = tk.Tk(); self.window.title('ChessBoard'); self.window.resizable(0,0)
+		self.board = board
+		self.validMoves = self.board.validMoves()
+		self.squareList=[]
+		# Initiating chessboard squares in squareList
+		for r in range(8):
+			for c in range(8):
+				label = tk.Label(self.window, bg=self.tiles[r*8+c], font=("Courier",40), width = 1, height = 1)
+				label.grid(row = 7-r, column = c, ipadx=11, ipady=3)
+				self.squareList.append(label)
+		self.drawGame()
+		self.window.mainloop()
+ 
+	def selected(self, event):
+		self.drawGame()
+		index = self.squareList.index(event.widget)
+		validMoves = self.validMoves[self.board.Pieces[self.board.turn].index(self.board.getPiece(index))]
+		for square in validMoves:
+			self.squareList[square].config(bg="dark sea green")
+			self.squareList[square].bind("<Button-1>", lambda x: self.moveSelect(x,index,validMoves))
+		return	
 
 
-# If the selection is a piece, 
-def unselected(event, board):
-	drawGame(board)
-	index = squareList.index(event.widget)
-	validMoves = board.validMoves(board.getPiece(index))
-	for square in validMoves:
-		squareList[square].config(bg="dark sea green")
-		squareList[square].bind("<Button-1>", lambda x: moveSelect(x,index,validMoves))
-	return	
+	def moveSelect(self, event,prevIndex,validMoves):
+		""" Tells board to move give piece and compute next array of validMoves """
+		self.board.move(prevIndex,self.squareList.index(event.widget))
+		self.drawGame()
+		self.validMoves = self.board.validMoves()
+		return
 
+	def drawGame(self):
+		""" Redraw and unbind every square. Draw pieces and make one player's clickable """
+		for i in range(len(self.squareList)):
+			label = self.squareList[i]
+			label.config(text="",bg=self.tiles[i])
+			label.bind("<Button-1>", '')
+		for piece in self.board.Pieces[WHITE]+self.board.Pieces[BLACK]:
+			label = self.squareList[piece.index]
+			label.config(text = piece)	
+		for piece in self.board.Pieces[self.board.turn]:
+			label = self.squareList[piece.index]
+			label.bind("<Button-1>", self.selected)
+		return
 
-def moveSelect(event,prevIndex,validMoves):
-	board.move(prevIndex,squareList.index(event.widget))
-	drawGame(board)
-	return
+GUI = GUI(board())	
 
-
-def initBoard(window, board):
-	global squareList
-	for r in range(8):
-		for c in range(8):
-			label = tk.Label(window, bg=tiles[r*8+c], font=("Courier",40), width = 1, height = 1)
-			label.grid(row = 7-r, column = c, ipadx=11, ipady=3)
-			squareList.append(label)
-	drawGame(board)
-	return
-
-
-def drawGame(board):
-	""" Redraw and unbind every square. Draw pieces and make one player's clickable """
-	for i in range(len(squareList)):
-		label = squareList[i]
-		label.config(text="",bg=tiles[i])
-		label.bind("<Button-1>", '')
-	for piece in board.Pieces[WHITE]+board.Pieces[BLACK]:
-		label = squareList[piece.index]
-		label.config(text = piece)	
-	for piece in board.Pieces[board.turn]:
-		label = squareList[piece.index]
-		label.bind("<Button-1>", lambda x: unselected(x, board))
-	return
-
-
-board = board()
-window = tk.Tk()
-window.title('ChessBoard')
-window.resizable(0,0)
-
-initBoard(window, board)
-	
-window.mainloop()
 
 
 
--- rules.py
+++ rules.py
@@ -16,25 +16,102 @@ class board:
 	turn = WHITE
 	enPassant = None
 
-
 	def __init__(self):
 		# White Pieces
-		self.Pieces[WHITE] += [king(4,WHITE), bishop(5,WHITE),rook(6,WHITE), castle(7,WHITE)]
-		self.Pieces[WHITE] +=  [castle(0,WHITE), rook(1,WHITE), bishop(2,WHITE), queen(3,WHITE)]
+		self.Pieces[WHITE] += [king(4,WHITE), castle(0,WHITE), castle(7,WHITE), bishop(5,WHITE)]
+		self.Pieces[WHITE] +=  [rook(6,WHITE), rook(1,WHITE), bishop(2,WHITE), queen(3,WHITE)]
 		for i in range(8): self.Pieces[WHITE].append(pawn(8+i,WHITE))
 
 		# Black Pieces
-		self.Pieces[BLACK] += [king(60,BLACK), bishop(61,BLACK),rook(62,BLACK), castle(63,BLACK)]
-		self.Pieces[BLACK] += [castle(56,BLACK), rook(57,BLACK), bishop(58,BLACK), queen(59,BLACK)]
+		self.Pieces[BLACK] += [king(60,BLACK), castle(56,BLACK), castle(63,BLACK), bishop(61,BLACK)]
+		self.Pieces[BLACK] += [rook(62,BLACK), rook(57,BLACK), bishop(58,BLACK), queen(59,BLACK)]
 		for i in range(8): self.Pieces[BLACK].append(pawn(48+i,BLACK))
 
-	def validMoves(self, piece):
-		return piece.validMoves(self)
+		# Initialize moves
+		self._calcMoves()
+
+	def _calcMoves(self):
+		self.enemyMoves = [piece.validMoves(self) for piece in self.Pieces[self.turn*-1][1:]]	
+		self.enemyMovesList = [move for sublist in self.enemyMoves for move in sublist]
+		self.allMoves = [piece.validMoves(self) for piece in self.Pieces[self.turn]]
+		self.friendMoves = self.allMoves[1:]
+	
+	def validMoves(self):
+		# If king is in check, restrict moves to getting him out or defending
+		kingIndex = self.Pieces[self.turn][0].index
+		if kingIndex in self.enemyMovesList:
+			# Find checking piece
+			for pieceIndex in range(len(self.enemyMoves)):
+				for move in self.enemyMoves[pieceIndex]:
+					if move == kingIndex:
+						piece = self.Pieces[self.turn*-1][pieceIndex+1]
+						break
+			# Remove moves that don't address check
+			if isinstance(piece, (pawn, rook)):
+				uncheckMoves = [piece.index]
+			else:
+				uncheckMoves = list(filter(lambda x: self._isBetween(x, self.Pieces[self.turn][0], piece), piece.validMoves(self))) + [piece.index]
+			validMoves = [self.allMoves[0]]
+			for moveList in self.friendMoves:
+				validMoves.append(list(filter(lambda x: x in uncheckMoves, moveList)))
+		else:
+			validMoves = self.allMoves
+		# If there is a pinned piece, restrict it's movement
+		king = self.Pieces[self.turn][0]
+		for piece in self.Pieces[self.turn*-1]:
+			if isinstance(piece, (castle, queen)):
+				if piece._xPos() == king._xPos() or piece._yPos() == king._yPos():
+					print('castle or queen is inline')
+					pinnedPiece = self.pinnedPiece(king, piece)
+					if pinnedPiece:
+						print('pinned piece')
+						pieceIndex = self.Pieces[self.turn].index(pinnedPiece)
+						validMoves[pieceIndex] = [move for move in pinnedPiece.validMoves(self) if move in piece.validMoves(self)]
+
+			elif isinstance(piece, (bishop, queen)):
+				if (piece._xPos()-king._xPos()) % (piece._yPos()-king._yPos()) == 0:
+					print('bishop or queen is diagonal')
+					pinnedPiece = self.pinnedPiece(king, piece)
+					if pinnedPiece:
+						print('pinned piece')
+						pieceIndex = self.Pieces[self.turn].index(pinnedPiece)
+						validMoves[pieceIndex] = [move for move in pinnedPiece.validMoves(self) if move in piece.validMoves(self)]
+		
+		return validMoves
+
+	def pinnedPiece(self, king, attacker):
+		piece = None
+		moveArray = list(filter(lambda x: self._isBetween(x, king, attacker), attacker.validMoves(self)))
+		for index in moveArray:
+			if attacker.isFriend(index, self): 
+				return None
+			if attacker.isEnemy(index, self): 
+				if piece:
+					return None
+				piece = self.getPiece(index)
+		return piece
+
+	def _isBetween(self, index, king, attacker):
+		sign = lambda x: x and (1, -1)[x < 0]
+		tmpPiece = piece(index, 'white')
+		xDir = sign(king._xPos()-attacker._xPos())
+		yDir = sign(king._yPos()-attacker._yPos())
+		output =  (sign(tmpPiece._xPos()-attacker._xPos()) == xDir) and (sign(tmpPiece._yPos()-attacker._yPos()) == yDir)
+		return output
+
+
 
 	def move(self, prevIndex, index):
 		self.getPiece(prevIndex).move(index, self)
 		self.turn *= -1
+		self._calcMoves()
 		return
+
+	def isCastle(self, index):
+		if self.isPiece(index):
+			piece = self.getPiece(index)
+			if isinstance(piece,castle):
+				return castle.firstMove
 
 	def isPiece(self, index):
 		for piece in (self.Pieces[WHITE]+self.Pieces[BLACK]):
@@ -88,6 +165,9 @@ class piece:
 			if piece.index == index:
 				return True
 		return False
+
+	def isEmpty(self, index, board):
+		return not (self.isFriend(index, board) or self.isEnemy(index, board))
 		
 	def _xPos(self, index=None):
 		""" xPos ranges from 1 to 8 for piece or given location
@@ -119,10 +199,11 @@ class pawn(piece):
 		validMoves = []
 		# Basic moves
 		moveIndex = self.index + self.color*8
-		if not self.isFriend(moveIndex, board):
+		if self.isEmpty(moveIndex, board):
 			validMoves.append(moveIndex)
-			if self.firstMove:
-				validMoves.append(moveIndex+self.color*8)
+			moveIndex += self.color*8
+			if self.firstMove and self.isEmpty(moveIndex, board):
+				validMoves.append(moveIndex)
 		# Capture moves
 		moveIndex = self.index + self.color*7
 		if self.isEnemy(moveIndex, board) and self._yPos(moveIndex) == self._yPos()+self.color:
@@ -157,10 +238,14 @@ class pawn(piece):
 			return super().__str__('♟')
 
 class castle(piece):
+
+	firstMove = True
+
 	def __init__(self, index, color):
 		super().__init__(index,color)
 
 	def move(self, index, board):
+		self.firstMove = False
 		super().move(index, board)
 		return
 
@@ -330,8 +415,7 @@ class queen(piece):
 
 class king(piece):
 
-	castleKing = True
-	castleQueen = True
+	firstMove = True
 
 	def __init__(self, index, color):
 		super().__init__(index,color)		
@@ -347,14 +431,38 @@ class king(piece):
 		# Remove invalid up/down moves, remove friendly moves
 		validMoves = list(filter(lambda x: x<64 and x>-1, validMoves))
 		validMoves = list(filter(lambda x: not(self.isFriend(x, board)), validMoves))
+		# Remove moves that put king in check
+		validMoves = list(filter(lambda x: x not in board.enemyMovesList, validMoves))
 
 		# Castling moves
-
-
+		if self.index not in board.enemyMovesList:
+			if self.firstMove == True:
+				if self.isEmpty(self.index+1, board) and self.isEmpty(self.index+2, board):
+					if self.index+1 and self.index+2 not in board.enemyMovesList:
+						if board.isCastle(self.index+3):
+							validMoves.append(self.index+2)
+				if self.isEmpty(self.index-1, board) and self.isEmpty(self.index-2, board) and self.isEmpty(self.index-3, board):
+					if self.index-1 and self.index-2 not in board.enemyMovesList:
+						if board.isCastle(self.index-4):
+							validMoves.append(self.index-2)
 		return validMoves
+
+	def move(self, index, board):
+		# Check for castling
+		if self.firstMove:
+			self.firstMove = False
+			if index == self.index+2:
+				board.getPiece(self.index+3).move(self.index+1, board)
+			if index == self.index-2:
+				board.getPiece(self.index-4).move(self.index-1, board)
+		super().move(index, board)
+
+		return
 
 	def __str__(self):
 		if self.color == WHITE:
 			return super().__str__('♔')
 		else:
 			return super().__str__('♚')
+
+
